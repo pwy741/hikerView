046f79d80c109a4a23517329fa8ae874009717a0
舞动的风 <615563901@qq.com>
2020-03-16 16:21:48 +0800

    tv

---
+++ recommend.js
@@ -0,0 +1,69 @@
+       {
+            "name": "央视🎈",
+            "lives": [
+        {
+          "name": "虎牙直播",
+          "url": "https://m.huya.com/g/2135"
+        },
+        {
+          "name": "喜马拉雅",
+          "url": "https://m.ximalaya.com/"
+        },
+        {
+          "name": "乐球吧",
+          "url": "https://m.leqiuba.com"
+        },
+        {
+          "name": "直播吧",
+          "url": "https://m.zhibo8.cc/?from=pc"
+        },
+        {
+          "name": "录像体育",
+          "url": "http://www.luxiangtiyu.com"
+        },
+        {
+          "name": "篮球直播",
+          "url": "http://m.lanqiu.tv"
+        },
+        {
+          "name": "360直播",
+          "url": "http://m.360zbz.com"
+        }, 
+         {
+          "name": "全国直播",
+          "url": "http://bddn.cn/zb.htm"
+        },
+         {
+          "name": "溜溜体育直播",
+          "url": "https://lltyzb.com/client/homem"
+        },
+         {
+          "name": "中国体育直播",
+          "url": "http://www.zhibo.tv/index/index"
+        },
+                 {
+          "name": "球趣直播",
+          "url": "http://qiuquzhibo.com"
+        },
+        {
+          "name": "爱看球直播",
+          "url": "https://m.aikanqiu.com"
+        },
+        {
+          "name": "柠檬直播",
+          "url": "http://m.ningmengbo.com"
+        },
+        {
+          "name": "JRS直播",
+          "url": "http://m.jrsnba.com"
+        },
+        {
+          "name": "飞速直播",
+          "url": "https://lltyzb.com/client/home"
+        },
+        {
+          "name": "自定义",
+          "url": "http://www.iyuji.cn/iyuji/s/MkJwenVHOEdBVE10L0ZqcmFibnBwZz09/1560953145493391"
+        }
+            ]
+        }
---
+++ search.js
@@ -0,0 +1,496 @@
+﻿{
+    "lives": [
+        {
+            "name": "爱搜资源",
+            "url": "https://www.aisouziyuan.com/"
+        },
+        {
+            "name": "云搜",
+            "url": "https://so.6hgr.top/"
+        },
+        {
+            "name": "我爱搜盘",
+            "url": "https://www.52sopan.com/"
+        },
+        {
+            "name": "云盘恶魔",
+            "url": "https://pan.09l.me/"
+        },
+        {
+            "name": "云盘精灵收费",
+            "url": "https://www.yunpanjingling.com/"
+        },
+        {
+            "name": "西林街",
+            "url": "http://www.xilinjie.com/"
+        },
+        {
+            "name": "胖次",
+            "url": "https://www.panc.cc/"
+        },
+        {
+            "name": "BTkitty",
+            "url": "http://btkitty.bid/"
+        },
+        {
+            "name": "A8BT",
+            "url": "http://a8bt.com/"
+        },
+        {
+            "name": "种子磁力",
+            "url": "http://zhongzicili.com/"
+        },
+        {
+            "name": "DiggBt",
+            "url": "http://diggbt.pw/"
+        },
+        {
+            "name": "torrent",
+            "url": "http://torrentsearchweb.co/"
+        },
+        {
+            "name": "KikiBT",
+            "url": "http://kikibt.net/"
+        },
+        {
+            "name": "BTlibrary",
+            "url": "http://btlibrary.me/"
+        },
+        {
+            "name": "Torrentkitty",
+            "url": "http://torrentkitty.ws/"
+        },
+        {
+            "name": "盘盘搜",
+            "url": "http://www.panpanso.com/"
+        },
+        {
+            "name": "去转盘",
+            "url": "http://www.quzhuanpan.com/"
+        },
+        {
+            "name": "网盘007",
+            "url": "http://wangpan007.com/"
+        },
+        {
+            "name": "盘多多",
+            "url": "http://www.panduoduo.net/"
+        },
+        {
+            "name": "盘搜",
+            "url": "http://www.pansou.com/"
+        },
+        {
+            "name": "云搜",
+            "url": "http://www.daysou.com/"
+        },
+        {
+            "name": "网盘搜索神器",
+            "url": "http://pan.muyi.so/"
+        },
+        {
+            "name": "搜百度盘",
+            "url": "http://www.sobaidupan.com/"
+        },
+        {
+            "name": "麦库搜索",
+            "url": "http://www.baidu10.net/"
+        },
+        {
+            "name": "特百度",
+            "url": "http://www.tebaidu.com/"
+        },
+        {
+            "name": "搜盘网",
+            "url": "http://www.soupan.info/"
+        },
+        {
+            "name": "我的盘",
+            "url": "http://www.wodepan.com/"
+        },
+        {
+            "name": "百度网盘之家",
+            "url": "http://wowenda.com/"
+        },
+        {
+            "name": "屌丝搜",
+            "url": "http://www.diaosisou.org/"
+        },
+        {
+            "name": "探索云盘资源",
+            "url": "http://tansuo233.com/"
+        },
+        {
+            "name": "小白盘",
+            "url": "https://www.xiaobaipan.com/"
+        },
+        {
+            "name": "求必搜",
+            "url": "http://www.qiubisou.xyz/"
+        },
+        {
+            "name": "磁力猫发布页",
+            "url": "https://www.cilimao.io/"
+        },
+        {
+            "name": "磁力鼠",
+            "url": "https://www.cilishu.world/"
+        },
+        {
+            "name": "telegram频道搜索",
+            "url": "http://www.sssoou.com/"
+        },
+        {
+            "name": "57分享百度云",
+            "url": "https://www.57fx.com/"
+        },
+        {
+            "name": "小可搜搜",
+            "url": "https://www.xiaokesoso.com/"
+        },
+        {
+            "name": "百度直链在线解析",
+            "url": "https://pan.baidusu.com/"
+        },
+        {
+            "name": "云盘助手",
+            "url": "https://yunpanzhushou.com/"
+        },
+        {
+            "name": "临时网盘",
+            "url": "https://send.firefox.com/"
+        },
+        {
+            "name": "磁力鸟",
+            "url": "https://ciliniao.cn/"
+        },
+        {
+            "name": "磁力搜",
+            "url": "https://bt.lansou.pw/"
+        },
+        {
+            "name": "大力盘",
+            "url": "https://www.dalipan.com/"
+        },
+        {
+            "name": "云盘狗",
+            "url": "http://www.yunpangou.com/"
+        },
+        {
+            "name": "如风搜",
+            "url": "http://www.rufengso.net/"
+        },
+        {
+            "name": "SooHub搜哈普",
+            "url": "https://www.soohub.com/"
+        },
+        {
+            "name": "盒子网盘",
+            "url": "http://pan.hezibuluo.com/"
+        },
+        {
+            "name": "超能搜",
+            "url": "http://www.chaonengso.com/"
+        },
+        {
+            "name": "盘窝窝",
+            "url": "https://www.panww.com/"
+        },
+        {
+            "name": "老王磁力",
+            "url": "https://laowang40.xyz/"
+        },
+        {
+            "name": "查种子",
+            "url": "https://chazhongzi01.com/"
+        },
+        {
+            "name": "种子猫",
+            "url": "https://www.zhongzikc.com/"
+        },
+        {
+            "name": "磁小鸭",
+            "url": "https://www.cixiaoya.club/"
+        },
+        {
+            "name": "fastsoso",
+            "url": "https://www.fastsoso.cn/"
+        },
+        {
+            "name": "BT-btt",
+            "url": "https://u3c3.com/"
+        },
+        {
+            "name": "小昭来啦",
+            "url": "https://www.xiaozhaolaila.com/"
+        },
+        {
+            "name": "磁力管家",
+            "url": "http://cili78.xyz/"
+        },
+        {
+            "name": "磁力宝",
+            "url": "http://www.cilibao.net/"
+        },
+        {
+            "name": "磁力狗",
+            "url": "http://www.ciligou.co/"
+        },
+        {
+            "name": "SkrBT",
+            "url": "https://skrbt.monster/"
+        },
+        {
+            "name": "聚BT",
+            "url": "https://jubt.net/cn/index.html"
+        },
+        {
+            "name": "番茄搜搜",
+            "url": "https://www.fqsousou.com/"
+        },
+        {
+            "name": "众人搜索",
+            "url": "http://wangpan.renrensousuo.com/"
+        },
+        {
+            "name": "优质吧",
+            "url": "http://uzi8.cn/"
+        },
+        {
+            "name": "盘搜搜",
+            "url": "http://www.pansoso.com/"
+        },
+        {
+            "name": "SOSO云盘",
+            "url": "https://www.sosoyunpan.com/"
+        },
+        {
+            "name": "超能搜",
+            "url": "http://www.chaonengsou.com/"
+        },
+        {
+            "name": "磁力搜",
+            "url": "https://magnetw.app/"
+        },
+        {
+            "name": "磁力熊",
+            "url": "https://www.cilixiong.com/"
+        },
+        {
+            "name": "Unblockit",
+            "url": "https://unblockit.ca/"
+        },
+        {
+            "name": "磁力哈",
+            "url": "http://xxx.ciliha.com/"
+        },
+        {
+            "name": "种子驴",
+            "url": "http://www.zhongzilv.org/"
+        },
+        {
+            "name": "SkrBT",
+            "url": "https://skrbt11.icu/"
+        },
+        {
+            "name": "雨花阁",
+            "url": "https://www.yhg15.xyz/"
+        },
+        {
+            "name": "超人搜索",
+            "url": "https://www.crsoso.pw/"
+        },
+        {
+            "name": "磁力王国",
+            "url": "https://yg5.monster/"
+        },
+        {
+            "name": "老王磁力",
+            "url": "https://laowang14.icu/"
+        },
+        {
+            "name": "磁力车",
+            "url": "https://gdsw.monster/"
+        },
+        {
+            "name": "磁力下载",
+            "url": "http://cilixiazai.top/"
+        },
+        {
+            "name": "磁力狗",
+            "url": "http://ciligou.top/"
+        },
+        {
+            "name": "BTMOO",
+            "url": "https://www.btmoo.com/"
+        }
+    ],
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
