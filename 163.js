54c4e2253b2a6692cce8c389222a31ab9f2e0b5d
舞动的风 <615563901@qq.com>
2020-05-13 23:21:32 +0800

    add 163.js.

---
+++ 163.js
@@ -0,0 +1,160 @@
+{
+    "lives": [
+        {
+            
+            "name": "云音乐飙升榜",
+            "num": "0001",
+            "url": "https://music.163.com/#/discover/toplist?id=19723756"
+        },
+        {
+            
+            "name": "云音乐新歌榜",
+            "num": "0002",
+            "url": "https://music.163.com/#/discover/toplist?id=3779629"
+        },
+        {
+            
+            "name": "网易原创歌曲榜",
+            "num": "0003",
+            "url": "https://music.163.com/#/discover/toplist?id=2884035"
+        },
+        {
+            
+            "name": "云音乐热歌榜",
+            "num": "0004",
+            "url": "https://music.163.com/#/discover/toplist?id=3778678"
+        },
+        {
+            
+            "name": "云音乐说唱榜",
+            "num": "0005",
+            "url": "https://music.163.com/#/discover/toplist?id=991319590"
+        },
+        {
+            
+            "name": "云音乐古典音乐榜",
+            "num": "0006",
+            "url": "https://music.163.com/#/discover/toplist?id=71384707"
+        },
+        {
+            
+            "name": "云音乐电音榜",
+            "num": "0007",
+            "url": "https://music.163.com/#/discover/toplist?id=1978921795"
+        },
+        {
+            
+            "name": "抖音排行榜",
+            "num": "0008",
+            "url": "https://music.163.com/#/discover/toplist?id=2250011882"
+        },
+        {
+            
+            "name": "新声榜",
+            "num": "0009",
+            "url": "https://music.163.com/#/discover/toplist?id=2617766278"
+        },
+        {
+            
+            "name": "云音乐ACG音乐榜",
+            "num": "0010",
+            "url": "https://music.163.com/#/discover/toplist?id=71385702"
+        },
+        {
+            
+            "name": "云音乐韩语榜",
+            "num": "0011",
+            "url": "https://music.163.com/#/discover/toplist?id=745956260"
+        },
+        {
+            
+            "name": "云音乐国电榜",
+            "num": "0012",
+            "url": "https://music.163.com/#/discover/toplist?id=10520166"
+        },
+        {
+            
+            "name": "英国Q杂志中文版周榜",
+            "num": "0013",
+            "url": "https://music.163.com/#/discover/toplist?id=2023401535"
+        },
+        {
+            
+            "name": "电竞音乐榜",
+            "num": "0014",
+            "url": "https://music.163.com/#/discover/toplist?id=2006508653"
+        },
+        {
+            
+            "name": "UK排行榜周榜",
+            "num": "0015",
+            "url": "https://music.163.com/#/discover/toplist?id=180106"
+        },
+        {
+            
+            "name": "美国Billboard周榜",
+            "num": "0016",
+            "url": "https://music.163.com/#/discover/toplist?id=60198"
+        },
+        {
+            
+            "name": "Beatport全球电子舞曲榜",
+            "num": "0017",
+            "url": "https://music.163.com/#/discover/toplist?id=3812895"
+        },
+        {
+            
+            "name": "KTV唛榜",
+            "num": "0018",
+            "url": "https://music.163.com/#/discover/toplist?id=21845217"
+        },
+        {
+            
+            "name": "iTunes榜",
+            "num": "0019",
+            "url": "https://music.163.com/#/discover/toplist?id=11641012"
+        },
+        {
+            
+            "name": "日本Oricon周榜",
+            "num": "0020",
+            "url": "https://music.163.com/#/discover/toplist?id=60131"
+        },
+        {
+            
+            "name": "Hit FM Top榜",
+            "num": "0021",
+            "url": "https://music.163.com/#/discover/toplist?id=120001"
+        },
+        {
+            
+            "name": "台湾Hito排行榜",
+            "num": "0022",
+            "url": "https://music.163.com/#/discover/toplist?id=112463"
+        },
+        {
+            
+            "name": "云音乐欧美热歌榜",
+            "num": "0023",
+            "url": "https://music.163.com/#/discover/toplist?id=2809513713"
+        },
+        {
+            
+            "name": "云音乐欧美新歌榜",
+            "num": "0024",
+            "url": "https://music.163.com/#/discover/toplist?id=2809577409"
+        },
+        {
+            
+            "name": "法国 NRJ Vos Hits 周榜",
+            "num": "0025",
+            "url": "https://music.163.com/#/discover/toplist?id=27135204"
+        },
+        {
+            
+            "name": "中国新乡村音乐排行榜",
+            "num": "0026",
+            "url": "https://music.163.com/#/discover/toplist?id=3112516681"
+        }
+    ]
+}
